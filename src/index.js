aa208af4c4bfaeea43f4f0e016091dbd5af1540c
chris <53133052+wiswis77@users.noreply.github.com>
2023-05-15 05:48:43 +0000

    remove [import app]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import ControlPanel from './ControlPanel'
 import reportWebVitals from './reportWebVitals';
 
